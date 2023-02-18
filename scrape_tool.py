59bae830a1aa77cfa8779e60479bfa29794f3e0a
LPP4 <114310099+J4DER4@users.noreply.github.com>
2023-02-18 21:37:11 +0200

    multi server scarape rdy

--- scrape_tool.py
+++ scrape_tool.py
@@ -3,58 +3,74 @@ from bs4 import BeautifulSoup
 
 server = "Phoenix"
 itemnbr = str(24888)
-targeturl = "https://universalis.app/market/" + itemnbr
+
 
 def scrape(server, itemnumber):
 
     r = requests.Session()
     r.cookies.update({"mogboard_server": server, "mogboard_last_selected_server": server})
+    targeturl = "https://universalis.app/market/" + str(itemnumber)
     c = r.get(targeturl)
     soup = BeautifulSoup(c.content, 'html.parser')
 
     data_list = []
     history = "NONE"
+    listings = True
+    history = True
 
     # ITEM NAME
     itemname = soup.find('div', class_="item_info")
-    data_list.append(soup.find('div', class_="item_info").text)
+    itemname = itemname.text.split()
+    data_list.append(" ".join(itemname[1:]))
 
     # CURRENT DATA
     data = soup.find('div', class_="tab-page tab-cw open")
-    print(data)
+    history = data.find('div', class_="cw-history")
 
     #CHECK FOR LISTINGS
-    if 'There are no listings' in data:
-        print("NO Listings")
+    if 'There are no listings' in data.text:
+        listings = False
 
-    for entry in data:
+    #CHECK FOR
+    if "Really rare!?" in history.text:
+        history = False
+
+    if listings:
+        for entry in data:
                 # Price_sinle
-        if (entry.find("td", class_="price-current")):
-            data_list.append(entry.find("td", class_="price-current").text)
-                # quanity
-        if (entry.find("td", class_="price-qty")):
-            data_list.append(entry.find("td", class_="price-qty").text)
-                # total price
-        if (entry.find("td", class_="price-total")):
-            data_list.append((entry.find("td", class_="price-total")).text)
-                    # History
-        if (entry.find('div', class_="cw-table cw-history")):
-            history = entry.find('div', class_="cw-history")
+            if (entry.find("td", class_="price-current")):
+                data_list.append(entry.find("td", class_="price-current").text)
+                    # quanity
+            if (entry.find("td", class_="price-qty")):
+                data_list.append(entry.find("td", class_="price-qty").text)
+                    # total price
+            if (entry.find("td", class_="price-total")):
+                data_list.append((entry.find("td", class_="price-total")).text)
+    else:
+        data_list.append('No listings')
+        data_list.append('No listings')
+        data_list.append('No listings')
 
     #HISTORY DATA
+    if history:
+        for entry in history:
+            # Price_sinle
+            if (entry.find("td", class_="price-current")):
+                data_list.append(entry.find("td", class_="price-current").text)
+            # quanity
+            if (entry.find("td", class_="price-qty")):
+                data_list.append(entry.find("td", class_="price-qty").text)
+            # total price
+            if (entry.find("td", class_="price-total")):
+                data_list.append((entry.find("td", class_="price-total")).text)
+            if (entry.find("td", class_="price-date")):
+                data_list.append((entry.find("td", class_="price-date")).text)
+    else:
+        data_list.append('No history')
+        data_list.append('No history')
+        data_list.append('No history')
+        data_list.append('No history')
 
-    for entry in history:
-        # Price_sinle
-        if (entry.find("td", class_="price-current")):
-            data_list.append(entry.find("td", class_="price-current").text)
-        # quanity
-        if (entry.find("td", class_="price-qty")):
-            data_list.append(entry.find("td", class_="price-qty").text)
-        # total price
-        if (entry.find("td", class_="price-total")):
-            data_list.append((entry.find("td", class_="price-total")).text)
-        if (entry.find("td", class_="price-date")):
-            data_list.append((entry.find("td", class_="price-date")).text)
     return data_list
 
 
@@ -68,5 +84,9 @@ def scrape(server, itemnumber):
     # 6 H QTY
     # 7 H TOTAL
     # 8 LAST SOLD
-print(scrape("Shiva", itemnbr))
+
+    # IF NO LISTINGS -> "No listings" x 3
+    # IF NO HISTORY -> "No history" x 4
+
+print(scrape("Alpha", itemnbr))
 
