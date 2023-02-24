6b48f645e33438f826e696ebe8df2ff9ad51fd54
Viljami Riihimäki <125034560+ViljamiRii@users.noreply.github.com>
2023-02-24 16:58:28 +0200

    Tidied up filters and made them more modifiable

---
+++ FF14_marketdata_remake/filters.py
@@ -0,0 +1,33 @@
+from data_scraper import item_list
+
+def filter_twintania_cheaper():
+    for item in item_list:
+        if int(item[1]) > int(item[4]):
+            item_list.remove(item)
+
+def filter_price_higher_than_cash(cash):
+    for item in item_list:
+        if int(item[1]) * int(item[2]) > int(item[4]) * int(item[2]):
+            item_list.remove(item)
+
+def filter_under_margin_trades():
+    for item in item_list:
+        if  int(item[4]) * int(item[2]) - int(item[1]) * int(item[2]) < 50000:
+            item_list.remove(item)
+
+def filter():
+
+    cash = 100000
+    try:
+        cash = int(input("Enter the amount of cash at hand (Default 100K):\n"))
+    except:
+        pass
+    if cash == "":
+        cash = 100000
+
+    print("Filtering out bad trades...\n")
+    
+    filter_twintania_cheaper()
+    filter_price_higher_than_cash(cash)
+    filter_under_margin_trades()
+    
--- FF14_marketdata_remake/main.py
+++ FF14_marketdata_remake/main.py
@@ -1,47 +1,6 @@
 from imports import *
 from data_scraper import *
-
-
-def filter(trade_margin):
-
-    print("Filtering out bad trades...\n")
-
-    index_list = []
-    filtered_list = item_list[:]
-    for item_index in range(0, len(item_list)):
-
-        # if price is higher on twintania delete the item
-        if int(item_list[item_index][1]) > int(item_list[item_index][4]):
-            index_list.append(item_list[item_index])
-    
-    for removal in index_list:
-        filtered_list.remove(removal)
-    index_list.clear()
-        
-    
-    filtered_list2 = filtered_list[:]
-    for item_index in range(0, len(filtered_list)):
-
-        # if price is sold is more than five times smaller it's probably a bait
-        if int(filtered_list[item_index][1]) * 5 < int(filtered_list[item_index][4]):
-            index_list.append(filtered_list[item_index])
-            
-    for removal in index_list:
-        filtered_list2.remove(removal)
-    index_list.clear()
-    
-    filtered_list3 = filtered_list2[:]
-    for item_index in range(0, len(filtered_list2)):
-
-        # if the profit is under {margin} delete the item
-        item_difference = (int(filtered_list2[item_index][4]) - int(filtered_list2[item_index][1])) * int(filtered_list2[item_index][2])
-        if item_difference < trade_margin:
-                index_list.append(filtered_list2[item_index])
-
-    for removal in index_list:
-        filtered_list3.remove(removal)            
-
-    return filtered_list3
+from filters import *
 
 def main():
 
@@ -67,18 +26,10 @@ def main():
     download_all_sites(sites)
     download_time = time.time() - start
     print(f"It took {download_time:.2f} seconds to complete the data scraping\n")
-    margin = 20000
-    try:
-        margin = int(input("Enter the wanted minimum trade margin (default 20000):\n"))
-    except:
-        pass
-    if margin == "":
-        margin = 20000
 
-    filtered_list = filter(margin)
-    
+    filter()
     with open("FF14_marketdata_remake/trades.txt", "w") as f:
-        for item in filtered_list:
+        for item in item_list:
             f.write(f"Item: {item[0]}, Price: {item[1]}, Quantity: {item[2]}, Server: {item[3]}, Twintania market price: {item[4]}, URL: {item[5]}\n")
             
     f.close()
